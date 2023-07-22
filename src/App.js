8a84b145bb5fc43565ff05434df69746d8104b06
Maruf Ahmed <marufsarkar512@gmail.com>
2023-07-22 15:42:00 +0600

    T1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <Job salary={12000} position="Junior SDE" company="Google" />
-      <Job salary={10000} position="Project Manager" company="Netflix" />
+      <h1 className="name"> Shadow </h1>
     </div>
   );
 }
-
-const Job = (data) => {
-  return (
-    <div>
-      {<h1>{data.company}</h1>}
-      {<h2>{data.position}</h2>}
-      {<h2>{data.salary}</h2>}
-    </div>
-  );
-};
 
 export default App;
